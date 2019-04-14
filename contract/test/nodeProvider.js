c6fa7d22cafbcde2786b75b3b735c21e2239cc56
Nicolas Mahe <nicolas@mahe.me>
2019-04-14 20:36:16 +1000

    Add tests

--- contract/test/nodeProvider.js
+++ contract/test/nodeProvider.js
@@ -7,7 +7,7 @@ const NodeProvider = artifacts.require('NodeProvider')
 const truffleAssert = require('truffle-assertions')
 
 contract('NodeProvider', async (accounts) => {
-  let contract = null
+  let nodeProvider = null
   const owner = accounts[0]
   const other = accounts[1]
   const nodes = accounts.slice(2, 12)
@@ -19,15 +19,24 @@ contract('NodeProvider', async (accounts) => {
     for (let i = 0; i < nodes.length; i++) {
       await nodeRegistry.register(sid, { from: nodes[i] })
     }
-    contract = await NodeProvider.new(nodeRegistry.address, { from: owner })
+    nodeProvider = await NodeProvider.new(nodeRegistry.address, { from: owner })
   })
 
   describe('pickNodes', async () => {
     it('should pick unique nodes', async () => {
-      const nodes = await contract.pickNodes(sid, 5)
+      const nodes = await nodeProvider.pickNodes(sid, 5)
       console.log('nodes', nodes)
       assert.equal(nodes.length, 5)
       assert.equal(nodes.length, nodes.filter((v, i, a) => a.indexOf(v) === i).length)
+    })
+  })
+
+  describe('verifyNodes', async () => {
+    it('should return true', async () => {
+      assert.isTrue(await nodeProvider.verifyNodes(sid, nodes))
+    })
+    it('should return false', async () => {
+      assert.isFalse(await nodeProvider.verifyNodes(sid, [other]))
     })
   })
 })
---
+++ contract/test/nodeRegistry.js
@@ -0,0 +1,43 @@
+/* eslint-env mocha */
+/* global contract, artifacts */
+
+const assert = require('chai').assert
+const NodeRegistry = artifacts.require('NodeRegistry')
+const truffleAssert = require('truffle-assertions')
+
+contract('NodeRegistry', async (accounts) => {
+  let nodeRegistry = null
+  const owner = accounts[0]
+  const other = accounts[1]
+  const node1 = accounts[2]
+  const node2 = accounts[3]
+  const sid = 0
+
+  before(async () => {
+    nodeRegistry = await NodeRegistry.new({ from: owner })
+  })
+
+  describe('register', async () => {
+    it('register one node with no stake', async () => {
+      await nodeRegistry.register(sid, { from: node1 })
+      assert.isTrue(await nodeRegistry.isRegistered(sid, node1))
+    })
+    it('register another node with a stake', async () => {
+      await nodeRegistry.register(sid, { from: node2, value: 100 })
+      assert.isTrue(await nodeRegistry.isRegistered(sid, node2))
+      assert.equal((await nodeRegistry.stake(sid, node2)).toNumber(), 100)
+    })
+  })
+
+  describe('cancel', async () => {
+    it('cancel one node', async () => {
+      await nodeRegistry.cancel(sid, { from: node1 })
+      assert.isFalse(await nodeRegistry.isRegistered(sid, node1))
+    })
+    it('should one node with stake', async () => {
+      await nodeRegistry.cancel(sid, { from: node2 })
+      assert.isFalse(await nodeRegistry.isRegistered(sid, node2))
+      // TODO: check that stake is send back.
+    })
+  })
+})
